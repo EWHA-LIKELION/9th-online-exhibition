5b15d9f68f155e4ab61977b77bce197411519dd3
Taeeun Kim <55427367+xodms0309@users.noreply.github.com>
2021-12-16 14:32:43 +0900

    Chore: init project (#1)
    * Initialize project using Create React App
    
    * chore: init project
    
    * chore: set global style

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { createGlobalStyle } from 'styled-components';
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div>hello world</div>
+    </>
+  );
+}
+const GlobalStyle = createGlobalStyle`
+  body{
+    max-width: 425px;
+    width: 100%;
+    min-height: 100vh;
+    margin: 0 auto;
+  }
+`;
+export default App;
