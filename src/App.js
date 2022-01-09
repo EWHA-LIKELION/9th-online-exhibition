45c242aec3baff4510a780fa43c33f3580910895
park ye jin <dyddyd0514@naver.com>
2022-01-09 12:53:06 +0900

    [add] background to main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import './App.css';
+import { Route, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import Main from './pages/Main';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div>hello world</div>
+      <Router />
     </>
   );
 }
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/detail" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
 const GlobalStyle = createGlobalStyle`
   ${reset};
   *{
@@ -21,7 +35,9 @@ const GlobalStyle = createGlobalStyle`
     width: 100%;
     min-height: 100vh;
     margin: 0 auto;
+    font-family: 'Noto Sans KR', sans-serif;
   }
+
   
 `;
 
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Main = () => {
-  return <div></div>;
+  return <Container></Container>;
 };
 
 export default Main;
+
+const Container = styled.div`
+  padding: 0px 36px;
+  height: 100%;
+  background-image: url(${process.env.PUBLIC_URL + 'main-background.png'});
+  background-size: contain;
+  background-repeat: no-repeat;
+`;
