483b59a49c1e08b21928d164bbab2a8e9f71b317
Taeeun Kim <55427367+xodms0309@users.noreply.github.com>
2022-01-09 13:20:24 +0900

    feat: create header (#4)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
 import { createGlobalStyle } from 'styled-components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import reset from 'styled-reset';
-
+import Main from './pages/Main';
+import Detail from './pages/Detail';
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div>hello world</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/main" element={<Main />} />
+          <Route path="/detail" element={<Detail />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
@@ -21,6 +27,7 @@ const GlobalStyle = createGlobalStyle`
     width: 100%;
     min-height: 100vh;
     margin: 0 auto;
+  background:#000000;
   }
   
 `;
